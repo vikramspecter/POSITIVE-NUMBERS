e41e48877b9adfdf9cbe1edd1f0e9d3788635902
vikramspecter <88393359+vikramspecter@users.noreply.github.com>
2021-08-11 10:36:20 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,64 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+enter how many numbers you want to eanter in the list3
+enter the particular number1
+[1]
+>>> 2
+2
+>>> 3
+3
+>>> 
+>>> 
+>>> 
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+enter how many numbers you want to eanter in the list3
+enter the particular number1
+[1]
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+enter how many numbers you want to eanter in the list3
+enter the particular number1
+enter the particular number2
+enter the particular number3
+[1, 2, 3]
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+enter how many numbers you want to enter in the list4
+enter the particular number1
+enter the particular number-1
+enter the particular number2
+enter the particular number-2
+[1, -1, 2, -2]
+the positive nos are 1
+the negative nos are -1
+the positive nos are 2
+the negative nos are -2
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+enter how many numbers you want to enter in the list4
+enter the particular number1
+enter the particular number-1
+enter the particular number2
+enter the particular number-2
+[1, -1, 2, -2]
+the positive numbers are [1, 2]
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+enter how many numbers you want to enter in the list4
+enter the particular number1
+enter the particular number2
+enter the particular number3
+enter the particular number4
+[1, 2, 3, 4]
+the positive numbers are 1
+the positive numbers are 2
+the positive numbers are 3
+the positive numbers are 4
+the positive list is  [1, 2, 3, 4]
+the negative list is []
+>>> 
+============= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python39/positive nos.py ============
+ENTER HOW MANY NUMBERS YOU WANT TO ENTER IN THE PARTICULAR LIST
